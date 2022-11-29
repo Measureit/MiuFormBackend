c971f170f027b82fc4a3a77d5a020a1715996ad7
Adam Nowak <adamus79@gmail.com>
2022-11-29 21:21:40 +0100

    Use sendinblue.

---
+++ src/emailsender.ts
@@ -0,0 +1,15 @@
+import { Observable } from "rxjs";
+
+export interface EmailMessage {
+    from: string;
+    to: string[];
+    subject: string;
+    plainContent: string;
+    reportName: string;
+    report: string; //base64
+    //reportData: string;
+}
+
+export interface EmailSender {
+    send(options: any, email: EmailMessage): Observable<any>;
+}
