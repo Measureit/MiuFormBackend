582872097f7d9a9b2657b789e9626dea96ea6b6d
Adam Nowak <adamus79@gmail.com>
2022-12-13 17:41:19 +0100

    remove dotenv

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,5 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
+//import * as dotenv from 'dotenv';
+//dotenv.config();
 import * as express from 'express';
 import { Request, Response } from 'express';
 import { first } from 'rxjs';
