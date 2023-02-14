a4d1c28067f9c9f3968e2e43a51f5e462784d556
Adam Nowak <adam.nowak@hotmail.com>
2023-02-14 20:09:49 +0100

    Update npms.

--- client/app/core/services/archive-report.service.ts
+++ client/app/core/services/archive-report.service.ts
@@ -22,22 +22,41 @@ export class ArchiveReportGeneratorService {
         private domSanitizer: DomSanitizer) {
     }
 
-    getFileNameForReport(factory: FactoryInfoConfig, report: Report): string {
-        return report._id; //todo
+    toFolderAndFile(n: string): string {
+        return n;
+    }
+
+    getFileNameForReport(report: Report): string {
+        return this.toFolderAndFile(report.productName + report.dateOfCreation.toLocaleString());
+    }
+
+    createFolderName(factories: FactoryInfoConfig[], factoryId: string): string {
+        const factory = factories.find(f => f._id === factoryId);
+        if (factory !== undefined) {
+            return this.toFolderAndFile(factory.shortName);
+        }
+        return this.toFolderAndFile(factoryId);
     }
 
     generateZip(factories: FactoryInfoConfig[], reports: Report[]): Observable<Blob> {
         const reportsGroupByFactory = this.groupByKey(reports, (r: Report) => r.factoryInfoId);
+
+        const toZipStruct = Array.from( reportsGroupByFactory )
+            .map(([key, value]) => ({ key, value }))
+            .map(v => ({ folder: this.createFolderName(factories, v.key),
+                reports: v.value.map(r => ({ name: this.getFileNameForReport(r), report: r}))}));
+
         const jsZip = new JSZip();
 
         reportsGroupByFactory.forEach((groupItemReports: Report[], key: string) => {
-            const factory = factories.find(f => f._id === key);
 
-            const jsZipFolder = jsZip.folder(factory.shortName);
+
+            const jsZipFolder = jsZip.folder(this.createFolderName(factories, key));
+            const jsZipFolder1 = jsZip.folder(this.createFolderName(factories, key));
 
             groupItemReports.forEach((report: Report) =>
             {
-                const fileName = this.getFileNameForReport(factory, report);
+                const fileName = this.getFileNameForReport(report);
 
                 this.reportGeneratorService.generatePdf(report);
 
